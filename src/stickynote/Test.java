ca2f8e6a2a9c946634244186800edf6f0c45b936
Yousef Rayyan <y.real.02@gmail.com>
2019-08-06 12:05:04 +0300

    Routing is done with some GUI touches

---
+++ src/stickynote/Test.java
@@ -0,0 +1,10 @@
+package stickynote;
+
+import javax.swing.*;
+
+public class Test {
+    private JPanel panel1;
+    private JPasswordField passwordField1;
+    private JPasswordField passwordField2;
+    private JButton button1;
+}
---
+++ src/stickynote/Tester.java
@@ -0,0 +1,10 @@
+package stickynote;
+
+import javax.swing.*;
+
+public class Tester {
+    private JTextField textField1;
+    private JPanel panel1;
+    private JButton button1;
+    private JPasswordField passwordField1;
+}
